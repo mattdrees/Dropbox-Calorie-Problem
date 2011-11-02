aa49a01386e4af2a0732719654e604dd724fa7b2
Matt Drees <matt.drees@ccci.org>
2011-11-02 12:06:24 -0400

    Parameterize the benchmark, and tweak

--- src/test/java/com/mattdrees/dropboxcalories/Benchmark.java
+++ src/test/java/com/mattdrees/dropboxcalories/Benchmark.java
@@ -1,20 +1,63 @@
 package com.mattdrees.dropboxcalories;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import com.google.caliper.Param;
 import com.google.caliper.Runner;
 import com.google.caliper.SimpleBenchmark;
 
+/**
+ * Benchmark results posted to 
+ * http://microbenchmarks.appspot.com/run/matt.drees@gmail.com/com.mattdrees.dropboxcalories.Benchmark
+ */
 public class Benchmark extends SimpleBenchmark {
 
-	Problem largeProblem;
+	public enum ExecutorServiceStrategy
+    {
+	    CACHED_THREAD_POOL {
+            @Override
+            ExecutorService build()
+            {
+                return Executors.newCachedThreadPool();
+            }
+        },
+	    SINGLE_THREAD {
+            @Override
+            ExecutorService build()
+            {
+                return Executors.newSingleThreadExecutor();
+            }
+        };
+	    
+	    abstract ExecutorService build();
+    }
+
+    Problem largeProblem;
 	Problem mediumProblem;
 	Problem smallProblem;
+	
+	@Param RoundBuildingStrategy roundStrategy;
+    
+	@Param ExecutorServiceStrategy executorStrategy;
+	
+	private ExecutorService executorService;
+	
 	
 	
 	@Override
 	protected void setUp() throws Exception {
-	    mediumProblem = new Problem();
+	    executorService = executorStrategy.build();
+	    
+	    buildMediumProblem();
+	    buildSmallProblem();
+        buildLargeProblem();
+	}
+
+    private void buildMediumProblem()
+    {
+        mediumProblem = new Problem();
         mediumProblem.items.add(new Item("free-lunch", 802));
         mediumProblem.items.add(new Item("mixed-nuts", 421));
         mediumProblem.items.add(new Item("orange-juice", 143));
@@ -27,27 +70,37 @@ public class Benchmark extends SimpleBenchmark {
         mediumProblem.items.add(new Item("riding-scooter", -42));
         mediumProblem.items.add(new Item("rock-band", -195));
         mediumProblem.items.add(new Item("playing-drums", -295));
-        
-
-	    smallProblem = new Problem();
+    }
+    
+    private void buildSmallProblem()
+    {
+        smallProblem = new Problem();
         smallProblem.items.add(new Item("red-bull", 140));
-        smallProblem.items.add(new Item("pushup", -10));
-        smallProblem.items.add(new Item("hopscotch", -130));
-        
+        smallProblem.items.add(new Item("hopscotch", -140));
+    }
+
+    private void buildLargeProblem()
+    {
         largeProblem = new Problem();
         for (int i = 0; i < 23; i++)
         {
-        	largeProblem.items.add(new Item("food" + i, 27));
+            largeProblem.items.add(new Item("food" + i, 27));
         }
         for (int i = 0; i < 27; i++)
         {
-        	largeProblem.items.add(new Item("excercise" + i, -23));
+            largeProblem.items.add(new Item("excercise" + i, -23));
         }
-	}
+    }
 	
-	public void timeLargeProblemWithNoSolution(int reps) throws InterruptedException, ExecutionException
+    @Override
+    protected void tearDown() throws Exception
+    {
+        executorService.shutdown();
+    }
+    
+	public void timeLargeProblemWithLargeSolution(int reps) throws InterruptedException, ExecutionException
 	{
-		ProblemSolver solver = new ProblemSolver(largeProblem);
+		ProblemSolver solver = new ProblemSolver(largeProblem, roundStrategy, executorService);
 		for (int i = 0; i < reps; i++)
 		{
 			solver.solve();
@@ -56,7 +109,7 @@ public class Benchmark extends SimpleBenchmark {
 	
 	public void timeMediumProblemWithSolution(int reps) throws InterruptedException, ExecutionException
 	{
-		ProblemSolver solver = new ProblemSolver(mediumProblem);
+		ProblemSolver solver = new ProblemSolver(mediumProblem, roundStrategy, executorService);
 		for (int i = 0; i < reps; i++)
 		{
 			solver.solve();
@@ -65,7 +118,7 @@ public class Benchmark extends SimpleBenchmark {
 	
 	public void timeSmallProblemWithSolution(int reps) throws InterruptedException, ExecutionException
 	{
-		ProblemSolver solver = new ProblemSolver(smallProblem);
+		ProblemSolver solver = new ProblemSolver(smallProblem, roundStrategy, executorService);
 		for (int i = 0; i < reps; i++)
 		{
 			solver.solve();
